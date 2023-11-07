ceb29cd1c90a4c566012e9e55841a38b2407b0ae
elliesgithub <149676919+elliesgithub@users.noreply.github.com>
2023-11-07 20:25:04 +0000

    Optimisation of initial code

--- milestone_2.py
+++ milestone_2.py
@@ -1,8 +1,14 @@
 import random 
+
+#list of words to be chosen from
 word_list = [ "Raspberry", "Strawberry", "Mango", "Passionfruit", "Orange" ]
+# randomly selects a word from the word_list
 word = random.choice(word_list)
 
+#prompt for the user to enter a single letter of their choosing
 guess = input("Enter a single letter:")
+
+#check if the guess is a single letter from the alphabet
 if len(guess) == 1 and guess.isalpha() :
     print("Good guess!")
 else:
